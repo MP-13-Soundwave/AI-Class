4e9103b7b51abd560db4ba5ab556bd89b38a546b
MP-13-Soundwave <99447910+MP-13-Soundwave@users.noreply.github.com>
2022-02-11 13:32:41 +1100

    Movement

--- Assets/Scripts/AIMovement.cs
+++ Assets/Scripts/AIMovement.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class AIMovement : MonoBehaviour
 {
+    /*create variable   
+    place data in variable
+    use variable*/
+    public GameObject position0;
+    public GameObject position1;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,24 @@ public class AIMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+
+        if (transform.position.x < position0.transform.position.x)
+        {
+            //move right
+            Vector2 AIPostion = transform.position;
+            AIPostion.x += (1 * Time.deltaTime);
+            transform.position = AIPostion;
+        }
+        else 
+        {
+            //move left
+            Vector2 AIPostion = transform.position;
+            AIPostion.x -= (1 * Time.deltaTime);
+            transform.position = AIPostion;
+        }
+        //Vector2 directionToPos0 = (Vector2)(position0.transform.position - transform.position);
     }
+
+
 }
